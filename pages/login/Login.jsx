427f80328074ac9716c919017fd961d1b8d65b87
Ebi Abaje <eabaje@outlook.com>
2022-10-10 01:39:12 +0100

    commit@10-10-2022-4

--- pages/login/Login.jsx
+++ pages/login/Login.jsx
@@ -1,6 +1,6 @@
 import { useContext, useRef } from "react";
 import "../../styles/login.module.css";
-import { loginCall } from "../../apiCalls";
+import { loginCall } from "../apiCalls";
 import { AuthContext } from "../../context/AuthContext";
 import { CircularProgress } from "@material-ui/core";
 
